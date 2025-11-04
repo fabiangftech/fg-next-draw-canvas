1a155b708d6463a21919ed785a5a5ba8d7c3e610
fabiangftech <fabian.gf.tech@gmail.com>
2025-11-04 18:35:02 -0300

    chore: add title to documentation

---
+++ __examples__/docs-fg-next-draw-canvas/src/components/fg-title/FGTitle.tsx
@@ -0,0 +1,12 @@
+import './FGTitle.css'
+function FGTitle(){
+    return (
+        <div className={"fg-title"}>
+            <h1>fg-next-draw-canvas</h1>
+        </div>
+    );
+}
+
+
+
+export default FGTitle;
--- __examples__/docs-fg-next-draw-canvas/src/templates/home/Home.tsx
+++ __examples__/docs-fg-next-draw-canvas/src/templates/home/Home.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import {FgnDrawCanvasComponent, FgnToolbarComponent, FgnZoomComponent} from "fg-next-draw-canvas";
+import FGTitle from "../../components/fg-title/FGTitle";
 
 function Home() {
     return (
         <div className="App">
+            <FGTitle/>
             <FgnToolbarComponent/>
             <FgnDrawCanvasComponent/>
             <FgnZoomComponent/>
