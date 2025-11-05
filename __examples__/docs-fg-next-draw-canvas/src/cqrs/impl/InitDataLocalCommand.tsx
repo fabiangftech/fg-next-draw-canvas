e643361847abcb20f1c19d66f7a8e79824c1ccc6
fabiangftech <fabian.gf.tech@gmail.com>
2025-11-05 18:22:34 -0300

    chore: add init

---
+++ __examples__/docs-fg-next-draw-canvas/src/cqrs/impl/InitDataLocalCommand.tsx
@@ -0,0 +1,12 @@
+import {Command} from "../Command";
+
+export class InitDataLocalCommand implements Command<void, null> {
+
+    constructor() {
+
+    }
+
+    execute(data: void): null {
+        return null;
+    }
+}
